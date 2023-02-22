6ee80252262ded28cc91fcdf418f5d76a3d71cde
Efren A. Mora <53714269+efrenmo@users.noreply.github.com>
2023-02-21 20:54:53 -0500

    added save and retrieve high-score functionality

--- scoreboard.py
+++ scoreboard.py
@@ -12,20 +12,26 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("White")
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, -100)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
-        self.write(art.GAME_OVER, align=ALIGNMENT)
 
     def increase_score(self):
         self.score +=1
-        self.clear()
         self.update_scoreboard()
 
